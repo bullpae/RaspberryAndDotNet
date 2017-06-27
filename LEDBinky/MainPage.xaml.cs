4ef7430a9b6dad01070e3a97f80f2f3a6c035111
student <student@M802>
2017-06-27 11:07:50 +0900

    Day 1 Source

---
+++ LEDBinky/MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+
+namespace LEDBinky
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        GpioController gpio;
+        GpioPin ledPin;
+        GpioPin swPin;
+        DispatcherTimer tmrBlink;
+        bool isBlink = true;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            gpio = GpioController.GetDefault();
+
+            ledPin = gpio.OpenPin(5);
+            ledPin.Write(GpioPinValue.High);
+            ledPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            swPin = gpio.OpenPin(4);
+            swPin.SetDriveMode(GpioPinDriveMode.Input);
+            swPin.ValueChanged += SwPin_ValueChanged;
+            swPin.DebounceTimeout = TimeSpan.FromMilliseconds(30); // 0.03 sec 이내 변화시
+          
+            tmrBlink = new DispatcherTimer();
+            tmrBlink.Interval = TimeSpan.FromMilliseconds(500); // 0.5 sec
+            tmrBlink.Tick += TmrBlink_Tick;
+            tmrBlink.Start(); // Start Timer
+
+            //GpioController gpio = GpioController.GetDefault();
+
+            //if (gpio == null)
+            //{
+            //    return;
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    using (GpioPin pin = gpio.OpenPin(5))
+            //    {
+            //        pin.Write(GpioPinValue.High);
+            //        pin.SetDriveMode(GpioPinDriveMode.Output);
+            //    }
+            //}
+        }
+
+        private void SwPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                // switch down
+                //ToggleLED();
+
+                // Cross Thread 문제가 발생!!
+                if (isBlink == true)
+                {
+                    tmrBlink.Stop(); 
+                }
+                else
+                {
+                    tmrBlink.Start();
+                }
+
+                isBlink = !isBlink;
+            }
+            //throw new NotImplementedException();
+        }
+
+        private void TmrBlink_Tick(object sender, object e)
+        {
+            ToggleLED();
+
+            //ledPin.SetDriveMode(GpioPinDriveMode.Output);
+            //throw new NotImplementedException();
+        }
+
+        private void ToggleLED()
+        {
+            if (ledPin.Read() == GpioPinValue.High)
+            {
+                ledPin.Write(GpioPinValue.Low);
+            }
+            else
+            {
+                ledPin.Write(GpioPinValue.High);
+            }
+        }
+    }
+}
---
+++ SonicSensor/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+
+namespace SonicSensor
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        GpioController gpio;
+        GpioPin triggerPin, echoPin;
+        DispatcherTimer tmrTrigger;
+
+        Stopwatch stopWatch = new Stopwatch();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            gpio = GpioController.GetDefault();
+            triggerPin = gpio.OpenPin(23);
+            echoPin = gpio.OpenPin(24);
+
+            triggerPin.Write(GpioPinValue.Low);
+            triggerPin.SetDriveMode(GpioPinDriveMode.Output);
+            
+            echoPin.SetDriveMode(GpioPinDriveMode.Input);
+            echoPin.ValueChanged += EchoPin_ValueChanged;
+
+            tmrTrigger = new DispatcherTimer();
+            tmrTrigger.Interval = TimeSpan.FromMilliseconds(500); // 0.5 sec
+            tmrTrigger.Tick += TmrTrigger_Tick; ;
+            tmrTrigger.Start(); 
+
+        }
+
+        private void TmrTrigger_Tick(object sender, object e)
+        {
+            ManualResetEventSlim mre = new ManualResetEventSlim();
+
+            // 트리거 발생
+            triggerPin.Write(GpioPinValue.High);
+            //Task.Delay(1); // 10마이크로 세크가 필요해서 다른 것을 사용
+            mre.Wait(TimeSpan.FromMilliseconds(0.01));
+            triggerPin.Write(GpioPinValue.Low);
+
+            //throw new NotImplementedException();
+        }
+
+        private void EchoPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.RisingEdge)
+            {
+                // 펄스의 시작
+                stopWatch.Reset();
+                stopWatch.Start();
+            }
+            else
+            {
+                stopWatch.Stop();
+                double width = stopWatch.Elapsed.TotalSeconds;
+                double distance = width * 17000;
+                Debug.WriteLine($"측정거리:{distance}cm");
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
